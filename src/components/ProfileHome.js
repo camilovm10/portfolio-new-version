8e269748142890a528c1807b337931c73098a8c3
Camilo Vasquez <camilovasquez9738@hotmail.com>
2020-11-12 14:42:30 -0500

    link de youtube fixed

--- src/components/ProfileHome.js
+++ src/components/ProfileHome.js
@@ -102,7 +102,7 @@ export default function ProfileHome() {
                     <Subtitle> Front-end Developer </Subtitle>
                     <IconContainer>
                         <a href="https://www.instagram.com/camilovasqz/" target="_blank"><i className="fab fa-instagram"></i></a>
-                        <a href="https://www.facebook.com/hoteldanncarltonmedellin/" target="_blank"><i class="fab fa-youtube-square"></i></a>
+                        <a href="https://www.youtube.com/user/camilovm2008" target="_blank"><i class="fab fa-youtube-square"></i></a>
                         <a href="https://www.linkedin.com/in/juan-camilo-vasquez-mira-3612911ab/" target="_blank"><i class="fab fa-linkedin"></i></a>
                         <a href="https://github.com/camilovm10/" target="_blank"><i class="fab fa-github-square"></i></a>
                     </IconContainer>
